7a02788fc8d224629a96f0026522a179aff4a917
sudoghut <sudospace@gmail.com>
2022-02-14 20:11:38 -0500

    cata. no. change to lowercase

--- pages/index.vue
+++ pages/index.vue
@@ -37,7 +37,7 @@
             <v-list>
               <span v-html="search_status"></span>
               <v-list-item v-for="record in data_model" :key="record.unique_id">
-                  <v-card-title class="headline"><a :href="'./page?id='+record.unique_id" target="_blank">{{record.catalogue_number_res.toLowerCase()}}</a>&nbsp;&nbsp;&nbsp;&nbsp;{{record.description}}</v-card-title>
+                  <v-card-title class="headline"><a :href="'./page?id='+record.unique_id" target="_blank">{{record.catalogue_number_res}}</a>&nbsp;&nbsp;&nbsp;&nbsp;{{record.description}}</v-card-title>
               </v-list-item>
             </v-list>
           </v-row></v-card-text>
